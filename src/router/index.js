3f4e94cf2361e5f7c9981b674857d7a754712879
ricardo.jiang <ricardo.jiang@vipshop.com>
2021-09-15 13:00:49 +0800

    2-3 项目基础代码编写

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+]
+
+const router = createRouter({
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
